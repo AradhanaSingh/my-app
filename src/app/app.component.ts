3950aa7fcb89cfeedae003d1859b91dfe642b285
Aradhana Singh <aru@Aradhanas-MacBook-Pro.local>
2018-06-17 16:50:56 -0700

    adding first component as an inline component template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,15 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  //templateUrl: './app.component.html',
+  //styleUrls: ['./app.component.css']
+  template: `
+  <div><h1> {{title}}</h1>
+       <div> My first component </div>
+  </div>
+  `
+
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Product Management Portal';
 }
