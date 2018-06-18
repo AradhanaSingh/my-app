34d27957a6070aea6cfb8932b9a6a86fc5c82d29
Aradhana Singh <aru@Aradhanas-MacBook-Pro.local>
2018-06-18 14:10:50 -0700

    added style to root component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   //templateUrl: './app.component.html',
-  //styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css']
   template: `
   <div><h1> {{title}}</h1>
        <pm-products></pm-products>
