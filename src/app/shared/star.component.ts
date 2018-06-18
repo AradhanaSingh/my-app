f866ce0413a9d529b6a9dc77f82e1743f797e231
Aradhana Singh <aru@Aradhanas-MacBook-Pro.local>
2018-06-18 15:21:41 -0700

    we see 5 star rating all the time

--- src/app/shared/star.component.ts
+++ src/app/shared/star.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input, ngOnChanges } from '@angular/core';
 
 @Component({
     selector: 'pm-star',
@@ -6,7 +6,10 @@ import { Component } from '@angular/core';
     styleUrls: ['star.component.css']
 })
 
-export class StarComponent {
+export class StarComponent implements ngOnChanges {
+    // nested component exposes a property it can use to receive input from its container using aptlt named decorator "input decorator"
+    // we want rating number to be passes into the nested component.
+    // container component then passes data to the nested component by setting this property with property binding
     rating: number = 4;
     startWidth: number;
 
