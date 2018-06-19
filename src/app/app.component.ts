87b67a4d925c1e69c918ee1544796f64e047fe27
Aradhana Singh <aru@Aradhanas-MacBook-Pro.local>
2018-06-19 15:06:04 -0700

    adding a service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
+import { ProductService } from '/Users/aru/code/my-app/src/app/products/product.service.ts'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
   template: `
   <div><h1> {{title}}</h1>
        <pm-products></pm-products>
   </div>
   `
+  //providers: [ProductService]
 
 })
 export class AppComponent {
